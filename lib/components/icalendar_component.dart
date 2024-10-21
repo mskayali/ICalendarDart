79cc80b7336747a9d3be06a6b9b57bc3867c5769
Mehmet Şeref Kayalı <mskayali@yandex.com>
2024-10-21 22:35:37 +0300

    Revert "no message"
    This reverts commit 83a593d1d53fdba2ad83e50e84e93d882aab5ac7.

--- lib/components/icalendar_component.dart
+++
@@ -1,9 +0,0 @@
-// Abstract class for calendar components
-abstract class ICalendarComponent {
-  String formatDateTime(DateTime dateTime) {
-    return '${dateTime.toUtc().toIso8601String().replaceAll('-', '').replaceAll(':', '').split('.').first}Z';
-  }
-
-  String serialize();
-  Map<String, dynamic> toJson();
-}
